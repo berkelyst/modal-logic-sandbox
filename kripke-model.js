d6423c0972ef42ab637a6201dfc5d130a0aec97c
berkelyst <nonprofessional2011@gmail.com>
2025-05-27 18:21:43 +0100

    Update kripke-model.js

--- kripke-model.js
+++ kripke-model.js
@@ -11,3 +11,11 @@ function isBoxPTrue(world, proposition) {
 
 console.log("□p est vrai en w1 :", isBoxPTrue("w1", "p"));
 console.log("□p est vrai en w2 :", isBoxPTrue("w2", "p"));
+function isDiamondPTrue(world, proposition) {
+  const accessibleWorlds = model[world].accessible;
+  return accessibleWorlds.some(w => model[w].propositions.includes(proposition));
+}
+
+// Exemples
+console.log("◇p est vrai en w1 :", isDiamondPTrue("w1", "p")); // true
+console.log("◇p est vrai en w3 :", isDiamondPTrue("w3", "p")); // false
